039cbfb24743a2aaf636642a047725b6b8425b09
rapetisekhar <113880531+rapetisekhar@users.noreply.github.com>
2024-07-15 16:53:30 +0530

    Update jenkins-install.sh

--- jenkins-server/jenkins-install.sh
+++ jenkins-server/jenkins-install.sh
@@ -6,7 +6,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install java-17-amazon-corretto-devel -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins

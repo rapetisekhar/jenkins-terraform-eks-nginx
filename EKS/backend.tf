8c2f2afc5222a1aa1ee1719e2eafff7eee88a25d
rapetisekhar <113880531+rapetisekhar@users.noreply.github.com>
2024-07-15 17:19:59 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "bond0077"
-    key            = "eks/terraform.tfstate"
+    
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "eks-terraform-lock"

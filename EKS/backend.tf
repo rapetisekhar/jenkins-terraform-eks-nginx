58928c7b8978506cdbcd0eaf241d39270edbe2dd
rapetisekhar <113880531+rapetisekhar@users.noreply.github.com>
2024-07-15 17:35:47 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "bond0077"
-    
-    region         = "ap-south-1"
-    encrypt        = false
-    dynamodb_table = "eks-terraform-lock"
-  }
-}
+
